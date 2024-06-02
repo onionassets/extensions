2a9118d0b6923fd6462eb491e418c7353d123911
stfu <69149087+onionassets@users.noreply.github.com>
2024-06-03 01:32:49 +0300

    Update extract account id-1.0.user.js
    added new bugs for more comfortable use

--- extract account id-1.0.user.js
+++ extract account id-1.0.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         extract account id
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.4
 // @description  Скрипт для автоматического вывода ID аккаунта. Также скрипт создает кнопку для перехода на страницу со всеми привязками этого человека, используя вместо никнейма ID аккаунта, что упрощает действия логера (аналог шаблонов от SetHP Script, но сразу с ID аккаунта, что будет чуть удобнее для такой задачи, как проверка привязок).
 // @author       vk.com/okeyflexer
 // @match        https://arizonarp.logsparser.info/*
@@ -10,8 +10,8 @@
 
 (function() {
     'use strict';
-
-    let firstAccountID = null;
+    // новый костыль заместо первой строки в логах
+    let targetAccountID = null;
 
     // Функция для поиска и добавления ID аккаунта
     function addAccountID() {
@@ -28,13 +28,17 @@
                 if (idMatch) {
                     let accountID = idMatch[1];
 
-                    if (!firstAccountID) {
-                        firstAccountID = accountID;
-                    }
-
                     let codeElement = document.createElement('code');
                     codeElement.textContent = ` ID аккаунта: ${accountID}`;
                     dataCell.appendChild(codeElement);
+
+                    // Проверяем наличие одного никнейма в строке
+                    let rowText = row.innerText.trim();
+                    let playerNames = rowText.match(/Игрок \S+/g); // Ищем все упоминания "Игрок <никнейм>"
+
+                    if (playerNames && playerNames.length === 1 && !targetAccountID) {
+                        targetAccountID = accountID;
+                    }
                 }
             }
         });
@@ -46,14 +50,14 @@
     function createButton() {
         const buttonContainer = document.querySelector('.filter-buttons');
 
-        if (buttonContainer && firstAccountID) {
+        if (buttonContainer && targetAccountID) {
             const button = document.createElement('button');
             button.innerHTML = 'Привязки';
             button.className = 'btn btn-success';
             button.style.marginRight = '10px';
             button.onclick = (event) => {
                 event.preventDefault();
-                const url = `https://arizonarp.logsparser.info/?server_number=30&type%5B%5D=vk_attache&type%5B%5D=vk_deattach&type%5B%5D=telegram_attach&type%5B%5D=telegram_deattach&type%5B%5D=acmail&type%5B%5D=googleauth_enable&type%5B%5D=googleauth_disable&type%5B%5D=mail_enable&type%5B%5D=mail_disable&type%5B%5D=mail&type%5B%5D=password&type%5B%5D=vk_attach&type%5B%5D=vk_detach&type%5B%5D=googleauth_attach&type%5B%5D=googleauth_detach&sort=desc&player=${firstAccountID}`;
+                const url = `https://arizonarp.logsparser.info/?server_number=30&type%5B%5D=vk_attache&type%5B%5D=vk_deattach&type%5B%5D=telegram_attach&type%5B%5D=telegram_deattach&type%5B%5D=acmail&type%5B%5D=googleauth_enable&type%5B%5D=googleauth_disable&type%5B%5D=mail_enable&type%5B%5D=mail_disable&type%5B%5D=mail&type%5B%5D=password&type%5B%5D=vk_attach&type%5B%5D=vk_detach&type%5B%5D=googleauth_attach&type%5B%5D=googleauth_detach&sort=desc&player=${targetAccountID}`;
                 window.open(url, '_blank');
             };
 
