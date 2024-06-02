b29352400412763b4207af78a850923196010d70
stfu <69149087+onionassets@users.noreply.github.com>
2024-06-02 20:09:09 +0300

    Add files via upload

---
+++ logs export-1.0.user.js
@@ -0,0 +1,71 @@
+// ==UserScript==
+// @name         logs export
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Скрипт для экспорта логов со страницы с логами
+// @author       vk.com/okeyflexer
+// @match https://arizonarp.logsparser.info/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Функция для создания кнопки
+    function createButton() {
+        // Находим контейнер для кнопок по указанному селектору
+        const buttonContainer = document.querySelector('.filter-buttons');
+
+        // Если контейнер найден, создаем кнопку и добавляем её в контейнер
+        if (buttonContainer) {
+            const button = document.createElement('button');
+            button.innerHTML = 'Выгрузить в txt';
+            button.className = 'btn btn-success';
+            button.style.marginLeft = '10px';
+            button.onclick = exportLogs;
+
+            buttonContainer.appendChild(button);
+        } else {
+            console.error('Контйнер не найден');
+        }
+    }
+
+    // Функция для экспорта логов
+    function exportLogs() {
+        const logs = [];
+        // Находим все строки таблицы логов
+        const rows = document.querySelectorAll('table tbody tr');
+
+        // Проходим по каждой строке и извлекаем данные
+        rows.forEach(row => {
+            const cells = row.querySelectorAll('td');
+            if (cells.length > 0) {
+                const dateTime = cells[0].innerText.trim();
+                const message = cells[1].innerText.trim();
+                logs.push(`${dateTime}\t${message}`);
+            }
+        });
+
+        const logText = logs.join('\n');
+        const blob = new Blob([logText], {type: 'text/plain'});
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = 'logs.txt';
+        link.click();
+    }
+
+    // Проверка загрузки страницы и наличия контейнера для кнопок
+    function checkLoaded() {
+        const container = document.querySelector('.filter-buttons');
+        if (container) {
+            createButton();
+        } else {
+            // Если контейнер не найден, повторяем проверку через 1 секунду
+            setTimeout(checkLoaded, 2500);
+        }
+    }
+
+    window.addEventListener('load', function() {
+        checkLoaded();
+    });
+})();
