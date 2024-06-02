f5ac778b3705f0ba6e4667c1cb8ac6c4160a3054
stfu <69149087+onionassets@users.noreply.github.com>
2024-06-03 01:51:25 +0300

    Update extract account id-1.0.user.js
    added as many bugs as possible for more comfortable use

--- extract account id-1.0.user.js
+++ extract account id-1.0.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         extract account id
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.7
 // @description  Скрипт для автоматического вывода ID аккаунта. Также скрипт создает кнопку для перехода на страницу со всеми привязками этого человека, используя вместо никнейма ID аккаунта, что упрощает действия логера (аналог шаблонов от SetHP Script, но сразу с ID аккаунта, что будет чуть удобнее для такой задачи, как проверка привязок).
 // @author       vk.com/okeyflexer
 // @match        https://arizonarp.logsparser.info/*
@@ -10,7 +10,7 @@
 
 (function() {
     'use strict';
-    // новый костыль заместо первой строки в логах
+
     let targetAccountID = null;
 
     // Функция для поиска и добавления ID аккаунта
@@ -51,21 +51,39 @@
         const buttonContainer = document.querySelector('.filter-buttons');
 
         if (buttonContainer && targetAccountID) {
-            const button = document.createElement('button');
-            button.innerHTML = 'Привязки';
-            button.className = 'btn btn-success';
-            button.style.marginRight = '10px';
-            button.onclick = (event) => {
-                event.preventDefault();
-                const url = `https://arizonarp.logsparser.info/?server_number=30&type%5B%5D=vk_attache&type%5B%5D=vk_deattach&type%5B%5D=telegram_attach&type%5B%5D=telegram_deattach&type%5B%5D=acmail&type%5B%5D=googleauth_enable&type%5B%5D=googleauth_disable&type%5B%5D=mail_enable&type%5B%5D=mail_disable&type%5B%5D=mail&type%5B%5D=password&type%5B%5D=vk_attach&type%5B%5D=vk_detach&type%5B%5D=googleauth_attach&type%5B%5D=googleauth_detach&sort=desc&player=${targetAccountID}`;
-                window.open(url, '_blank');
-            };
+            let existingButton = document.querySelector('.filter-buttons .btn-success');
 
-            buttonContainer.appendChild(button);
-        } else {
-            console.error('Контейнер не найден или ID аккаунта не найден');
+            if (!existingButton) {
+                const button = document.createElement('button');
+                button.innerHTML = 'Привязки';
+                button.className = 'btn btn-success';
+                button.style.marginRight = '10px';
+                button.onclick = (event) => {
+                    event.preventDefault();
+                    const url = `https://arizonarp.logsparser.info/?server_number=30&type%5B%5D=vk_attache&type%5B%5D=vk_deattach&type%5B%5D=telegram_attach&type%5B%5D=telegram_deattach&type%5B%5D=acmail&type%5B%5D=googleauth_enable&type%5B%5D=googleauth_disable&type%5B%5D=mail_enable&type%5B%5D=mail_disable&type%5B%5D=mail&type%5B%5D=password&type%5B%5D=vk_attach&type%5B%5D=vk_detach&type%5B%5D=googleauth_attach&type%5B%5D=googleauth_detach&sort=desc&player=${targetAccountID}`;
+                    window.open(url, '_blank');
+                };
+
+                buttonContainer.appendChild(button);
+                console.log('Кнопка была добавлена.');
+            } else {
+                console.log('Кнопка уже существует.');
+            }
+        } else if (!buttonContainer) {
+            console.error('Контейнер не найден');
         }
     }
 
-    window.addEventListener('load', addAccountID);
+    // Функция для повторного добавления кнопки через определенные промежутки времени
+    function monitorButton() {
+        const interval = setInterval(() => {
+            console.log('Проверка наличия кнопки...');
+            createButton();
+        }, 1500);
+    }
+
+    window.addEventListener('load', () => {
+        addAccountID();
+        monitorButton();
+    });
 })();
